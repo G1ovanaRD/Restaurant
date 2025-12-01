91656b12175295f66a9824e15f5eba50af3cc72d
Daphish <ivan.davila.2003@gmail.com>
2025-11-30 20:07:35 -0600

    La descripción se carga al editar el platillo

--- resources/views/platillos-modifica.blade.php
+++ resources/views/platillos-modifica.blade.php
@@ -6,7 +6,7 @@
     <form method="POST" action="{{ route('platillos.update', $platillo->id) }}">
         @csrf
         <flux:input label="Nombre" placeholder="Nombre" wire:model='nombre' value="{{ $platillo->nombre }}"/>
-        <flux:textarea label="Descripción" placeholder="Descripción" wire:model='descripcion' value="{{ $platillo->descripcion }}"/>
+        <flux:textarea label="Descripción" placeholder="Descripción" wire:model='descripcion'>{{ $platillo->descripcion }}</flux:textarea>
         <flux:input label="Precio" placeholder="Precio" type="number" step="0.01" wire:model='precio' value="{{ $platillo->precio }}"/>
         <flux:input label="Categoría" placeholder="Categoría" wire:model='categoria' value="{{ $platillo->categoria }}"/>
         <flux:input label="Imagen" placeholder="Imagen" wire:model='imagen' value="{{ $platillo->imagen }}"/>
