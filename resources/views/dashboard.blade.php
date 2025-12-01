d1fb16f8885d788a5ec9518f4789c9ee3fc2e272
G1ovanaRD <a33998@alumnos.uaslp.mx>
2025-11-30 19:36:11 -0600

    Dashboard y header styles

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,18 +1,25 @@
 <x-layouts.app :title="__('Dashboard')">
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
-        <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+        <div class="flex">
+            <div class="w-1/2 flex flex-col justify-center px-24">
+                <h1 class="text-7xl font-semibold text-accent-secondary">{{ __('Welcome to Foody Dashboard') }}</h1>   
+                <div class="flex gap-4">
+                    <button>
+                        <a href="{{ route('platillos.index') }}" class="mt-6 inline-block rounded-lg bg-green-600 px-6 py-3 text-lg font-medium text-white hover:bg-green-700">
+                            {{ __('View Menu') }}
+                        </a>
+                    </button>  
+                    <button>
+                        <a href="{{ route('platillos.index') }}" class="mt-6 inline-block rounded-lg bg-black-food px-6 py-3 text-lg font-medium text-white hover:bg-[#2C2C2C]">
+                            {{ __('Book Table') }}
+                        </a>
+                    </button>        
+                </div>
             </div>
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-            </div>
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+            <div class="w-1/2 flex items-center justify-center">
+                <img src="{{ asset('images/banner.png') }}" alt="Food Delivery" />
             </div>
         </div>
-        <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-        </div>
+
     </div>
 </x-layouts.app>
